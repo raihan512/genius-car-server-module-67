8a3e77bfd92a8bc74d24c28e0bc06c0a7f712312
raihan512 <raihangazi1024@gmail.com>
2023-03-07 05:08:46 +0600

    mongodb added & services and single servie load done

--- index.js
+++ index.js
@@ -11,6 +11,40 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e4yec41.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const serviceCollection = client
+      .db("geniusCar")
+      .collection("serviceCollection");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+  } finally {
+  }
+}
+
+run().catch((error) => console.error);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
